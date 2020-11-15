95e4157a8369834ea7785f12c56cb9640e525133
Artem Kolin <artemkaxboy@gmail.com>
2020-11-15 13:45:46 +0700

    Adds Jacoco support

--- src/main/kotlin/com/artemkaxboy/springdatashowroom/dto/VersionDto.kt
+++ src/main/kotlin/com/artemkaxboy/springdatashowroom/dto/VersionDto.kt
@@ -5,5 +5,5 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(title = "Version", description = "Basic version information.")
 data class VersionDto(
 
-    val version: String,
+    val version: String = "",
 )
