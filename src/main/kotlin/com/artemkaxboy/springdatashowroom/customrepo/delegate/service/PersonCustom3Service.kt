2aef65429d41763d5d168b8a997cbd04e8c0f911
Artem Kolin <artemkaxboy@gmail.com>
2020-12-04 22:18:28 +0700

    Tests added

--- src/main/kotlin/com/artemkaxboy/springdatashowroom/customrepo/delegate/service/PersonCustom3Service.kt
+++ src/main/kotlin/com/artemkaxboy/springdatashowroom/customrepo/delegate/service/PersonCustom3Service.kt
@@ -12,17 +12,33 @@ class PersonCustom3Service(
     private val personCustom3StorageService: PersonCustom3StorageService,
 ) {
 
+    /**
+     * Generates and saves person into repository.
+     */
     fun generatePerson(): PersonCustom3 {
         return personCustom3StorageService.save(PersonCustom3(name = NameGenerator.generate()))
     }
 
+    /**
+     * Updates rating for person with given [personId].
+     */
     fun updateRating(personId: Long, rating: Int): Result<PersonCustom3> {
         return personCustom3StorageService.updateRatingById(personId, rating)
             ?.let { Result.success(it) }
             ?: Result.failure("Person (id=$personId) not found")
     }
 
+    /**
+     * Returns all people from repository.
+     */
     fun getAll(): List<PersonCustom3> {
         return personCustom3StorageService.findAll(Sort.by(PersonCustom3::id.name))
+    }
+
+    /**
+     * Deletes all people from repository.
+     */
+    fun clear() {
+        personCustom3StorageService.deleteAll()
     }
 }
--- src/main/kotlin/com/artemkaxboy/springdatashowroom/customrepo/extensions/service/PersonCustom2Service.kt
+++ src/main/kotlin/com/artemkaxboy/springdatashowroom/customrepo/extensions/service/PersonCustom2Service.kt
@@ -13,17 +13,33 @@ class PersonCustom2Service(
     private val personCustom2Repository: PersonCustom2Repository,
 ) {
 
+    /**
+     * Generates and saves person into repository.
+     */
     fun generatePerson(): PersonCustom2 {
         return personCustom2Repository.save(PersonCustom2(name = NameGenerator.generate()))
     }
 
+    /**
+     * Updates rating for person with given [personId].
+     */
     fun updateRating(personId: Long, rating: Int): Result<PersonCustom2> {
         return personCustom2Repository.updateRatingById(personId, rating)
             ?.let { Result.success(it) }
             ?: Result.failure("Person (id=$personId) not found")
     }
 
+    /**
+     * Returns all people from repository.
+     */
     fun getAll(): List<PersonCustom2> {
         return personCustom2Repository.findAll(Sort.by(PersonCustom2::id.name))
+    }
+
+    /**
+     * Deletes all people from repository.
+     */
+    fun clear() {
+        personCustom2Repository.deleteAll()
     }
 }
